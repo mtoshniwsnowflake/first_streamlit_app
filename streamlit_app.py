e5d2af15ca48829b8af59697fd239a6f686e6145
mtoshniwsnowflake <131236198+mtoshniwsnowflake@users.noreply.github.com>
2023-04-19 12:18:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title("First streamlit App") sauce
+streamlit.title("First streamlit App") 
 streamlit.header("Breakfast Meenu")
-streamlit.text("Omlette with Tomato")
+streamlit.text("Omlette with Tomato sauce")
 streamlit.text("Cabbage with Mayoneese ")
 streamlit.text("Perfact protien diet")
